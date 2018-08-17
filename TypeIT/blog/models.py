061fee7925315985fa51de249b747f742f886007
Schizarede <romsthirdyfrancisco@gmail.com>
2018-08-17 20:08:56 +0900

    'installed_octicons_and_finished_connecting_templates_urls'

--- TypeIT/blog/models.py
+++ TypeIT/blog/models.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author=models.ForeignKey('auth.User')
+    author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     text=models.TextField()
-    create_date=models.DateTimeField(default=timezone.now())
+    created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -18,16 +18,16 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse("psot_detail",kwarg={'pk':self.pk})
+        return reverse("post_detail",kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    post=models.ForeignKey('blog.Post',related_name='comments')
+    post=models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
     author= models.CharField(max_length=200)
     text=models.TextField()
-    create_date=models.DateTimeField(default=timezone.now())
+    created_date=models.DateTimeField(default=timezone.now)
     approve_comment=models.BooleanField(default=False)
 
     def approve(self):
